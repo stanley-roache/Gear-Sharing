20587d6e9e46b67926dd49ca921af6ad6cbda0ae
talor-hammond <talorjhammond@gmail.com>
2018-07-12 13:59:01 +1200

    lostpet component renders individual pet info

--- client/components/LostPet.jsx
+++ client/components/LostPet.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const LostPet = props => (
   <div className="column is-one-third has-text-bold">
-    {props.pet.species}
+    <div className="box has-text-left">
+      <img src={props.pet.photo} />
+      <div className='pet-name'>Name: {props.pet.name}</div>
+      <div className='pet-species'>Species: {props.pet.species}</div>
+      <div className='pet-owner'>Notify Owner</div>
+    </div>
   </div>
 )
 
