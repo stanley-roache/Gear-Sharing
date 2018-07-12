1b038d9ad8df9d69d46060f9db3b591a4268f3b2
talor-hammond <talorjhammond@gmail.com>
2018-07-13 10:26:19 +1200

    wrote found and foundpet component, added client route to found view

---
+++ client/components/Found.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import {connect} from 'react-redux'
+
+import FoundPet from 'react-redux'
+
+export const Found = props => {
+  return (
+    <div className='pets'>
+      <div className='is-size-1'> Found Pets </div>
+      <div className="columns is-multiline">
+        {props.foundPets.map(pet => (
+          <FoundPet key={pet.id} pet={pet} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+const mapStateToProps = ({foundPets}) => ({
+  foundPets
+})
+
+export default connect(mapStateToProps)(Found)
