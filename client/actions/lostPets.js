69308b53f0aef5c3b57aec67d8cb637f53e4610a
anton <anton.arus@gmail.com>
2018-07-12 13:56:34 +1200

    added receiving / adding actions for lostPets

---
+++ client/actions/lostPets.js
@@ -0,0 +1,17 @@
+const RECEIVE_LOST_PETS = 'RECEIVE_LOST_PETS'
+const ADD_LOST_PET = 'ADD_LOST_PET'
+
+
+export function receiveLostPets(lostPets) {
+    return {
+        type: RECEIVE_LOST_PETS, 
+        lostPets
+    }
+}
+
+export function addLostPet(lostPet) {
+    return {
+        type: ADD_LOST_PET,
+        lostPet
+    }
+}
