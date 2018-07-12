37b3782f81b6d1db3b84f550ad8338b4fb6735a6
phoenixjessiestryker <phoenixjstryker@gmail.com>
2018-07-13 10:52:30 +1200

    finished lost seed

--- seeds/populateLost.js
+++ seeds/populateLost.js
@@ -10,12 +10,12 @@ exports.seed = function(knex, Promise) {
           name: 'Pouya',
           species: 'Mouse',
           photo: 'https://cdn.pixabay.com/photo/2018/03/03/02/26/mouse-3194768__340.jpg',
-          user_id: 1
+          user_id: 2
         },
         {
           id: 2,
           name: 'GZA',
-          species: 'Ratboi',
+          species: 'Rat',
           photo: 'https://vignette.wikia.nocookie.net/poohadventures/images/6/64/Rufus_%28Kim_Possible%29.jpeg/revision/latest?cb=20140708203220',
           user_id: 1
         },
@@ -24,42 +24,42 @@ exports.seed = function(knex, Promise) {
           name: 'Ice Cube',
           species: 'Dog',
           photo: 'https://media.jumiadeals.com/ng_live/2c5421174c5ada2083a3b25.desktop-gallery-large.jpg',
-          user_id: 1
+          user_id: 3
         },
         {
           id: 4,
           name: 'Lil Peep',
           species: 'Lizard',
           photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Tiliqua_scincoides_scincoides.jpg/1200px-Tiliqua_scincoides_scincoides.jpg',
-          user_id: 1
+          user_id: 2
         },
         {
           id: 5,
           name: 'Hot Tuna',
           species: 'Fish',
           photo: 'http://agoras24.com/wp-content/uploads/2018/03/tuna.jpg',
-          user_id: 1
+          user_id: 3
         },
         {
           id: 6,
           name: 'Denzel Curry',
           species: 'Dog',
           photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Max_at_the_river2.jpg/220px-Max_at_the_river2.jpg',
-          user_id: 1
+          user_id: 2
         },
         {
           id: 7,
           name: '6ix9ine',
           species: 'Bird',
           photo: 'https://media.treehugger.com/assets/images/2011/10/lorikeet.jpg',
-          user_id: 1
+          user_id: 3
         },
         {
           id: 8,
           name: 'Slug Christ',
           species: 'Slug',
           photo: 'https://www.publicdomainpictures.net/pictures/110000/velka/banana-slug.jpg',
-          user_id: 1
+          user_id: 2
         },
         {
           id: 9,
@@ -68,6 +68,13 @@ exports.seed = function(knex, Promise) {
           photo: 'https://beafunmum.com/wp-content/uploads/2012/01/dreamstime_s_20093731.jpg',
           user_id: 1
         },
-      ])
+        {
+          id: 10,
+          name: 'XXXTentacion',
+          species: 'Possum',
+          photo: 'https://flyfishhunt.co.nz/wp-content/uploads/2016/09/IMG_6618-1-e1472796323791.jpg',
+          user_id: 2
+        },
+      ]);
     });
 };
--- seeds/populateUsers.js
+++ seeds/populateUsers.js
@@ -14,6 +14,20 @@ exports.seed = function(knex, Promise) {
           contact_details: 'Ring the bell ;)',
           hash: hashSync('Krang', saltRounds)
         }
+        {
+          id: 2,
+          user_name: 'haydenb',
+          email_address: 'hblummont1@gmail.com',
+          contact_details: 'Play RuPaul\'s Drag race theme loudly',
+          hash: hashSync('Krang', saltRounds)
+        }
+        {
+          id: 3,
+          user_name: 'chuck',
+          email_address: 'rich@imrich.co.usa',
+          contact_details: 'Send a driver',
+          hash: hashSync('Krang', saltRounds)
+        }
       ]);
     });
 };
