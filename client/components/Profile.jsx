a112cb54a51c35ded6bce1a0321eb0046a081e7a
Stanley Roache <stan.roache@gmail.com>
2018-07-20 11:37:27 +1200

    made profile.jsx stateful, profile keeps boolean in state to render or hide new tool form

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -1,20 +1,49 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {Route} from 'react-router-dom'
 
 import GearList from './GearList'
+import NewGearForm from './NewGearForm'
 
-const Profile = (props) => {
+class Profile extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      addingItem: false
+    }
+
+    this.cancelAdd = this.cancelAdd.bind(this)
+    this.renderForm = this.renderForm.bind(this)
+  }
+
+  cancelAdd() {
+    this.setState({
+      addingItem: false
+    })
+  }
+
+  renderForm() {
+    this.setState({
+      addingItem: true
+    })
+  }
+
+  render() {
     return (
-        <div>
-            <h2>{props.user.firstName}</h2>
-            <h2>{props.user.lastName}</h2>
-            <p>{props.user.email}</p>
+        <div className='profile-display'>
+            <h2>{this.props.user.firstName}</h2>
+            <h2>{this.props.user.lastName}</h2>
+            <p>{this.props.user.email}</p>
             <GearList />
-            <button>Add Gear Item</button>
+            {!this.state.addingItem && <button onClick={() => this.renderForm()}>Add Gear Item</button>}
+            {this.state.addingItem && <NewGearForm finish={() => this.cancelAdd()}/>}
         </div>
     )
+  }
 }
-
+  
+  // <Route exact path="/newItem" component={NewGearForm} />
 const mapStateToProps = (state) => (
     {
         auth: state.auth,
