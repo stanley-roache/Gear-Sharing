b8b507dc507c73d3b6e839be031da4c210a54a41
Stanley Roache <stan.roache@gmail.com>
2018-07-20 10:40:33 +1200

    added blank gearlist component, added button to profile component

---
+++ client/components/GearList.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const GearList = props => (
+  <div>GEARLIST PLACEHOLDER</div>
+)
+
+export default GearList
--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import {connect} from 'react-redux'
 
+import GearList from './GearList'
+
 const Profile = (props) => {
     return (
         <div>
             <h2>{props.user.firstName}</h2>
             <h2>{props.user.lastName}</h2>
             <p>{props.user.email}</p>
+            <GearList />
+            <button>Add Gear Item</button>
         </div>
     )
 }
