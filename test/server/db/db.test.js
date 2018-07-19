8158190f9ffa1a3f1bb8ca8125d0c2b92c83bd8e
hayden-blummont <hblummont@gmail.com>
2018-07-20 10:58:21 +1200

    complete user db tests

--- test/server/db/db.test.js
+++ test/server/db/db.test.js
@@ -1,4 +1,5 @@
 const gear = require('../../../server/db/gear')
+const users = require('../../../server/db/users')
 const env = require('./test-environment')
 
 let testDb = null
@@ -18,6 +19,14 @@ const fakeUser = {
   email_address: 'bill@murray.com'
 }
 
+const fakeNewUser = {
+  username: 'billyboi',
+  first_name: 'bill',
+  last_name: 'murray',
+  email_address: 'bill@murray.com',
+  password: 'klsjdgljsdg'
+}
+
 const fakeGearItem = {
   status: "Available",
   trustframework: "One",
@@ -31,6 +40,10 @@ const fakeGearItemUpdate = {
   name: 'Dyno Drilla'
 }
 
+const fakeUserUpdate = {
+  first_name: 'Ichabod'
+}
+
 test('addGear function returns an ID of type numbers', done => {
   gear.addGear(fakeGearItem, testDb)
     .then(ids => {
@@ -41,66 +54,95 @@ test('addGear function returns an ID of type numbers', done => {
     })
 })
 
-test('updateGear function causes one record to be changed', done => {
-  gear.updateGear(fakeGearItemUpdate, 1, testDb)
+test('updateGear function causes one record to be changed', () => {
+  return gear.updateGear(fakeGearItemUpdate, 1, testDb)
     .then(numUpdated => {
       expect(numUpdated).toBe(1)
-      done()
     })
 })
 
-test('getGear function retrieves all gear', done => {
-  gear.getGear(testDb)
+test('getGear function retrieves all gear', () => {
+  return gear.getGear(testDb)
     .then(gearArray => {
       expect(gearArray).toHaveLength(4)
-      done()
     })
     .catch(err => {
-      done(err)
+      expect(err).toBeFalsey()
     })
 })
 
+test('getGearByGearId function gets a gear by ID', () => {
+  return gear.getGearByGearId(1, testDb)
+    .then(gear => {
+      expect(gear).toBeTruthy()
+      expect(gear.id).toBe(1)
+    })
+})
 
+test('getGearWithUsers function joins the users and gear', () => {
+  const expectedKeys = [
+    'id',
+    'first_name',
+    'last_name',
+    'user_name',
+    'email_address',
+    'hash',
+    'status',
+    'trustframework',
+    'name',
+    'description',
+    'photo_url',
+    'user_id',
+  ]
+  gear.getGearWithUsers(testDb)
+    .then(actualArr => {
+      actualArr.forEach(actual => {
+        expectedKeys.forEach(key => {
+          expect(actual.hasOwnProperty(key)).toBeTruthy()
+        })
+      })
+    })
+})
 
+test('getUserByName function does what it says it does', () => {
+  return users.getUserByName('haydiggidydoc', testDb)
+    .then(users => {
+      expect(users.user_name).toEqual('haydiggidydoc')
+  })
+})
 
+test('getUserById function gets a user by their Id', () => {
+  return users.getUserById(2, testDb)
+    .then(users => {
+      expect(users.first_name).toEqual('Haydeen')
+    })
+})
 
+test('getUsers function returns users', () => {
+  return users.getUsers(testDb)
+    .then(users => {
+      expect(users).toHaveLength(4)
+    })
+})
 
+test('updateUser function causes one user to change', () => {
+  return users.updateUser(1, fakeUserUpdate, testDb)
+    .then(numChanged => {
+        expect(numChanged).toEqual(1)
+    })
+})
 
+test('createUser function creates a new user', () => {
+const {user_name, first_name, last_name, email_address, password} = fakeNewUser
+  return users.createUser(user_name, first_name, last_name, email_address, password, testDb)
+    .then(users => {
+      expect(users).toHaveLength(1)
+    })
+})
 
-
-// test('getLost pets db function', () => {
-//   return db.getLost(testDb)
-//     .then(lostPets => {
-//       expect(lostPets).toHaveLength(10)
-//     })
-// })
-
-// test('add a lost pet to db', () => {
-//   return db.addLost({name: 'Hayden'}, testDb)
-//     .then(lostPet => {
-//         expect(lostPet).toEqual([11])
-//         return testDb('lost')
-//           .then(lostPets => {
-//             expect(lostPets).toHaveLength(11)
-//             expect(lostPets[10].name).toBe('Hayden')
-//           })
-//         })
-//       })
-
-//     test('getFound pets db function', () => {
-//         return db.getFound(testDb)
-//           .then(foundPets => {
-//             expect(foundPets).toHaveLength(10)
-//           })
-//     })
-
-//       test('add a found pet to db', () => {
-//         return db.addFound(testFoundPet, testDb)
-//           .then(foundPet => {
-//               expect(foundPet).toEqual([11])
-//               return testDb('found')
-//                 .then(foundPets => {
-//                   expect(foundPets).toHaveLength(11)
-//                 })
-//               })
-//             })
+test('userExists function lets you know if a username is taken', () => {
+  return users.userExists('Diddly', testDb)
+    .then(userExists => {
+      expect(userExists).toBeTruthy()
+    })
+})
