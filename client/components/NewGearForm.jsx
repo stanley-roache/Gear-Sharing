827e0608e515f303ec780ebca50efec487146f72
Stanley Roache <stan.roache@gmail.com>
2018-07-20 11:45:33 +1200

    changed trustframework input to drop list

--- client/components/NewGearForm.jsx
+++ client/components/NewGearForm.jsx
@@ -12,7 +12,7 @@ export class NewGearForm extends React.Component {
       photoUrl: '',
       trustframework: "One",
     }
-    
+
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
@@ -29,40 +29,39 @@ export class NewGearForm extends React.Component {
     // addGearItem(this.state)
     this.props.finish()
   }
-  
+
   render() {
     return (
       <form className='new-gear-form' onSubmit={this.handleSubmit}>
         <label>
-          Name: 
-          <input onChange={this.handleChange} type="text" value={this.state.name} name='name'/>
+          Name:
+          <input onChange={this.handleChange} type="text" value={this.state.name} name='name' />
         </label>
-        <br/>
+        <br />
         <label>
-          Description: 
-          <input onChange={this.handleChange} type="text" value={this.state.description} name='description'/>
+          Description:
+          <input onChange={this.handleChange} type="text" value={this.state.description} name='description' />
         </label>
-        <br/>
+        <br />
         <label>
-          Photo URL: 
-          <input onChange={this.handleChange} type="text" value={this.state.photo_url} name='photoUrl'/>
+          Photo URL:
+          <input onChange={this.handleChange} type="text" value={this.state.photo_url} name='photoUrl' />
         </label>
-        <br/>
+        <br />
         <label>
-          lending conditions: 
-          <input onChange={this.handleChange} type="text" value={this.state.trustframework} name='trustframework'/>
+          Lending conditions: 
+          <select name='trustframework' onChange={this.handleChange}>
+            <option value='One'>Free Borrowing</option>
+            <option value='Two'>Upkeep Koha</option>
+            <option value='Three'>Conditional (contact to arrange)</option>
+          </select>
         </label>
-        <br/>
-        <input type="submit" value='add Gear'/>
+        <br />
+        <input type="submit" value='add Gear' />
         <button onClick={this.props.finish}>Cancel</button>
       </form>
     )
   }
-  // <label>
-  //   <select name='trustframework' onChange={this.handleChange}>
-  //   <input onChange={this.handleChange} type="select" value={this.state.photo_url} name='photoUrl'/>
-  //   </select>
-  // </label>
 }
 
 export default NewGearForm
