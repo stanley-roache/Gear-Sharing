eb58d7e9c9ceeb4d31b1aa2b1c2c0caeaee17743
reubenjh <rjharcourt@gmail.com>
2018-07-19 20:04:59 +1200

    cleaned up console.logs

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import {connect} from 'react-redux'
 
 const Profile = (props) => {
-    console.log(props)
     return (
         <div>
             <h2>{props.user.firstName}</h2>
