e47c6577c7745de8ac8c30e8da5e9147be0c3760
reubenjh <rjharcourt@gmail.com>
2018-07-20 10:57:45 +1200

    login/register gets gear for a user - profile jsx maps it

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -7,6 +7,11 @@ const Profile = (props) => {
             <h2>{props.user.firstName}</h2>
             <h2>{props.user.lastName}</h2>
             <p>{props.user.email}</p>
+            <ul>
+                {props.user.gear.map((item) => {
+                    return <li>{item.name} - {item.description} - {item.status} - {item.trustframework}</li>
+                })}
+            </ul>
         </div>
     )
 }
