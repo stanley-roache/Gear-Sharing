6aba874c9a183c7fcd647160bc6ccdeffadc5438
reubenjh <rjharcourt@gmail.com>
2018-07-21 07:06:03 +1200

    removed public/private placeholder routes

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -3,13 +3,6 @@ const {getSecret, handleError} = require('../auth/token')
 const router = require('express').Router()
 const dbgear = require('../db/gear')
 
-// no frontend setup to test back-end secret routes
-
-router.get('/public', (req, res) => {
-    console.log('public')
-    res.send('public')
-})
-
 router.use(
     (req, res, next) => {
         console.log('middleware');
@@ -25,11 +18,6 @@ router.use(
 )
 
 // all routes below here protected
-
-router.get('/secret', (req, res) => {
-    console.log('secret')
-    res.send('secret')
-})
 
 router.get('/gear'), (req, res) => {
     console.log('hitting gear route');
