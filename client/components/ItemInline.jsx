52f6b21036229c77990a8a0e493ee7f526aff6d3
Stanley Roache <stan.roache@gmail.com>
2018-07-20 15:22:15 +1200

    wrote basic ToolPool and InlineItem display components, these assume some connection to redux which is not implemented yet

---
+++ client/components/ItemInline.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const ItemInline = props => {
+  const {name, description, status} = props.item
+  return (
+    <div><h4>{name} - {description} - {status}</h4></div>
+  )
+}
+
+export default ItemInline
