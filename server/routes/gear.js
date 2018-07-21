ea8172386ecfd46fcd25b30b794d4718290356d7
Stanley Roache <stan.roache@gmail.com>
2018-07-21 12:23:20 +1200

    wrote first 3 wrotes, including unprotected get isngle and all gear items (UNPROTECTED), and add new gear item (PROTECTED)

--- server/routes/gear.js
+++ server/routes/gear.js
@@ -1,16 +1,62 @@
 const router = require('express').Router()
-const {getGear} = require('../db/gear')
+const verifyJWT = require('express-jwt')
+const { getSecret, handleError } = require('../auth/token')
+const { getUserByName } = require('../db/users')
+gearDB = require('../db/gear')
 
-router.get('/', (req, res) => {
-    getGear()
-        .then((gear) => {
-            res.status(200).json(gear)
-        })
-        // .catch(err => {
-        //     console.log('in catch block')
-        //     res.status(500).send({ message: 'error getting gear' })
-        // })
-        // TODO: error catching not working
+// public routes
+
+router.get('/single/:id', (req, res) => {
+  const gearID = req.params.id
+
+  gearDB.getGearByGearId(gearID)
+    .then(gear => {
+      res.json(gear)
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: 'error grabbing single item',
+        err
+      })
+    })
+})
+
+router.get('/all', (req, res) => {
+  gearDB.getGear()
+    .then(gearList => {
+      res.json(gearList)
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: 'error grabbing full gear list',
+        err
+      })
+    })
+})
+
+router.use(
+  verifyJWT({
+    secret: getSecret
+  }),
+  handleError
+)
+
+router.post('/new', (req, res) => {
+  const newItem = req.body
+
+  gearDB.addGear(newItem)
+    .then(ids => {
+      res.json({
+        gear: newItem,
+        id: ids[0]
+      })
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: 'error adding gear item',
+        err
+      })
+    })
 })
 
 module.exports = router
