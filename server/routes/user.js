613bb64773f921bf3f59aa405a51beaed5ba7c48
Stanley Roache <stan.roache@gmail.com>
2018-07-21 12:10:03 +1200

    removed old route

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,22 +4,6 @@ const { getSecret, handleError } = require('../auth/token')
 const { getUserByName } = require('../db/users')
 const { getGearByUserId } = require('../db/gear')
 
-// initial user info fetching route, to be removed
-
-// router.get('/:username', (req, res) => {
-//   let user = {}
-//   getUserByName(req.params.username)
-//     .then((userInfo) => {
-//       user = userInfo
-//       getGearByUserId(userInfo.id)
-//         .then((gearArray) => {
-//           user.gear = gearArray
-//           res.status(200).json(user)
-//         })
-//     })
-//     .catch(err => res.status(500).send({ message: 'error getting user info' }))
-// })
-
 router.use(
   verifyJWT({
     secret: getSecret
