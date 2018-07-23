1b31c79ea1a97be49cf677577faf3b04e521ac73
reubenjh <rjharcourt@gmail.com>
2018-07-24 11:41:41 +1200

    built login and reg

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import {connect} from 'react-redux'
-import {loginUser} from '../actions/login'
-
+import { connect } from 'react-redux'
+import { loginUser } from '../actions/login'
+import { Link } from 'react-router-dom'
 
 class Login extends React.Component {
   constructor(props) {
@@ -15,36 +15,50 @@ class Login extends React.Component {
   }
 
   updateDetails(e) {
-    this.setState({[e.target.name]: e.target.value})
+    this.setState({ [e.target.name]: e.target.value })
   }
 
   submit(e) {
     e.preventDefault()
-    let {user_name, password} = this.state
-    this.props.dispatch(loginUser({user_name, password}))
+    let { user_name, password } = this.state
+    this.props.dispatch(loginUser({ user_name, password }))
   }
 
   render() {
-    const {auth} = this.props
+    const { auth } = this.props
 
     return (
-      <form className="form box" onSubmit={this.submit}>
-        <h1 className="title is-2">Login</h1>
-        <hr />
-        {auth.errorMessage && <span className="has-text-danger is-large">{auth.errorMessage}</span>}
-        <label className="label is-large has-text-centered">Username
-          <input required className="input has-text-centered is-large is-fullwidth" placeholder="User Name" type="text" name="user_name" onChange={this.updateDetails}/>
-        </label>
-        <label className="label is-large has-text-centered">Password
-          <input required className="input has-text-centered is-large is-fullwidth" placeholder="Password" type="password" name="password" onChange={this.updateDetails}/>
-        </label>
-        <input className="button is-large is-fullwidth is-success" value='Login' type="submit" />
-      </form>
+      <div className='section'>
+        <div className='columns is-centered'>
+          <div className='column is-6'>
+
+            <form className='form' onSubmit={this.submit}>
+              <h1 className="title is-1 has-text-centered">Login</h1>
+              {auth.errorMessage
+                && <p className="has-text-danger is-large has-text-centered">{auth.errorMessage}</p>}
+              <hr />
+
+              <label className="label is-large">Username
+                <input required className="input is-large is-fullwidth" placeholder="Username" type="text" name="user_name" onChange={this.updateDetails} />
+              </label>
+
+              <label className="label is-large">Password
+                <input required className="input is-large is-fullwidth" placeholder="Password" type="password" name="password" onChange={this.updateDetails} />
+              </label>
+              <p className='is-small has-text-centered'>Don't have an account? <Link to='/register'>Create an account here</Link></p>
+
+              <hr />
+              <input className="submit button is-centered is-black is-large" value='Login' type="submit" />
+            </form>
+
+          </div>
+        </div>
+      </div>
     )
   }
 }
 
-const mapStateToProps = ({auth}) => {
+const mapStateToProps = ({ auth }) => {
   return {
     auth
   }
