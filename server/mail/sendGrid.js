8c132b24ab65d6d3b50c87e92da539afd84b8add
stanley-roache <stan.roache@gmail.com>
2018-07-24 10:57:52 +1200

    modified sendGrid api call to recieve object containing message, user and gear info, builds custom (currenlty looking raw) email

--- server/mail/sendGrid.js
+++ server/mail/sendGrid.js
@@ -7,15 +7,21 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const adminEmail = 'toolerino1@gmail.com'
 const requestSubject = 'new request for gear use'
 
-function sendRequest(item, requester) {
-  const requestBody = `Hey ${item.user_name}, ${requester.user_name} wants to use your ${item.name}, contact them at ${requester.email_address}`
+function sendRequest(details) {
+  const {item, requester, messageBody} = details
+  const requestBodyHTML = `
+  Hey ${item.user_name}, ${requester.user_name} wants to use your ${item.name},
+  <br><br>
+  User message: ${messageBody}
+  <br><br>
+  contact them at ${requester.email_address}`
   
   return new Promise((resolve, reject) => {
     const msg = {
       to: item.email_address,
       from: adminEmail,
       subject: requestSubject,
-      html: `<strong>${requestBody}</strong>`,
+      html: requestBodyHTML,
       generateTextFromHTML: true
     };
     sgMail.send(msg, (err, result) => {
