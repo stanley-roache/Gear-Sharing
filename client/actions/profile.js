192920b8a6cd341d951fee01cdb5d8de36d6c281
stanley-roache <stan.roache@gmail.com>
2018-07-24 16:16:16 +1200

    wrote thunk actions to manage profile edits in db and local state

---
+++ client/actions/profile.js
@@ -0,0 +1,34 @@
+import request from '../utils/api'
+
+function requestProfileEdit() {
+    return {
+        type: 'REQUEST_PROFILE_EDIT'
+    }
+}
+
+function succesfulProfileEdit(update) {
+    return {
+        type: 'SUCCESS_PROFILE_EDIT',
+        update
+    }
+}
+
+function errorProfileEdit(message) {
+    return {
+        type: 'ERROR_PROFILE_EDIT',
+        message
+    }
+}
+
+export function editProfileAction(profileUpdate) {
+    return dispatch => {
+        dispatch(requestProfileEdit())
+        return request('post', 'user/update', profileUpdate)
+            .then((res) => {
+                dispatch(succesfulProfileEdit(profileUpdate))
+            })
+            .catch(err => {
+                dispatch(errorProfileEdit(err.message))
+            })
+    }
+}
