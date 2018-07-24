c4c04408a801f92b901758290c67344d3b7cae5a
hayden-blummont <hblummont@gmail.com>
2018-07-25 10:09:57 +1200

    add colour links to all navbar links

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -20,19 +20,19 @@ class Nav extends React.Component {
             <p><Link className={this.props.location.pathname === "/toolpool" ? 'active' : ""} to="/toolpool">ALL TOOLS</Link></p>
           </div>
           <div className='column is-6'>
-            <h1 className='title is-3'><Link to="/">TOOLPOOL</Link></h1>
+            <h1 className={this.props.location.pathname === "/login" ? 'active title is-3' : "title is-3"}><Link to="/">TOOLPOOL</Link></h1>
           </div>
           <div className='column is-3'>
             {console.log(this.props)}
             {this.props.auth.isAuthenticated
               ? [
                 <div>
-                <Link to="/profile">PROFILE</Link>
+                <Link className={this.props.location.pathname === "/profile" ? 'active' : ""} to="/profile">PROFILE</Link>
                 <Link className='navSpace' to="/" onClick={this.props.logout}>LOGOUT</Link>
                 </div>
                 ]
                 : [
-                  <Link to='/login'>LOGIN</Link>
+                  <Link className={this.props.location.pathname === "/login" ? 'active' : ""} to='/login'>LOGIN</Link>
                 ]
               }
           </div>
