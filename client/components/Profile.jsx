f690f9be918588ddd6add89752a7aedf9c7b6a4a
reubenjh <rjharcourt@gmail.com>
2018-07-24 18:25:06 +1200

    style styule syetoheh;kle

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -71,7 +71,14 @@ class Profile extends React.Component {
           {/* {!this.props.auth.isAuthenticated && <Redirect to='/login'/>} */}
           {this.props.user.message && <span className="has-text-danger is-large">{this.props.user.message}</span>}
 
-          <div className='section'>
+          <div className='section profile'>
+
+            <div className='columns'>
+              <div className='column is-12 tab'>
+                <p className='is-small is-pulled-right tab'>Profile</p>
+                <p className='is-small is-pulled-right tab'>My Messages</p>
+              </div>
+            </div>
 
             <div className='columns is-multiline'>
 
@@ -81,56 +88,63 @@ class Profile extends React.Component {
 
               <div className='column is-6'>
                 <div className='columns is-multiline'>
-                  <div className='column is-8'>
+                  <div className='column is-12'>
                     <h1 className='title is-1'>@{this.props.user.username}</h1>
-                  </div>
-
-                  <div className='column is-4 tab'>
-                    <p className='is-small is-pulled-right tab'>Profile</p>
-                    <p className='is-small is-pulled-right tab'>My Messages</p>
                   </div>
 
                   <div className='column is-12'>
                     <h5 className='title is-5' id='name'>{this.props.user.firstName} {this.props.user.lastName}</h5>
                     <p>{this.props.user.email}</p>
                     <p className='rating'>Trust Rating: </p>
-                      <span className='rating'><StarRatingComponent
-                        name="rate1"
-                        starCount={5}
-                        value={this.hoverRating || this.rating}
-                        onStarClick={this.rate}
-                        onStarHover={this.changeHoverRating}
-                        onStarHoverOut={this.endHover}
-                        starColor='gold'
-                      />
+                    <span className='rating'><StarRatingComponent
+                      name="rate1"
+                      starCount={5}
+                      value={this.hoverRating || this.rating}
+                      onStarClick={this.rate}
+                      onStarHover={this.changeHoverRating}
+                      onStarHoverOut={this.endHover}
+                      starColor='gold'
+                    />
                     </span>
-                    <br/>
-                    
-                    <a className='button edit'>Edit Profile</a>
+                    <br />
+
+                    {!this.state.editingProfile && !this.state.addingItem
+                      && <a className='button button-pad' onClick={() => this.renderForm('editingProfile')}>Edit Profile</a>}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
 
-        <div className='section'>
-          <GearList />
-        </div>
+            <div className='profile-gear'>
+              <div className='level'>
+                <div className='level-left'>
+                  <div className='level-item'>
+                    <h1 className='title is-1'>MY TOOLS</h1>
+                  </div>
+                </div>
+                <div className='level-right'>
+                  <div className='level-item'>
+                    {!this.state.addingItem && !this.state.editingProfile
+                      && <button className='button is-black is-medium button-pad' onClick={() => this.renderForm('addingItem')}>Add Tool</button>}
+                  </div>
+                </div>
+              </div>
+              <hr />
 
-        <div className='section'>
-          {!this.state.addingItem && !this.state.editingProfile
-            && <button onClick={() => this.renderForm('addingItem')}>Add Gear Item</button>}
-          <div ref='addingItem'>
-            {this.state.addingItem
-              && <NewGearForm finish={() => this.setFalse('addingItem')} />}
+              <GearList />
+
+              <div ref='addingItem'>
+                {this.state.addingItem
+                  && <NewGearForm finish={() => this.setFalse('addingItem')} />}
+              </div>
+
+
+              <div ref='editingProfile'>
+                {this.state.editingProfile
+                  && <EditProfileForm onFinish={() => this.setFalse('editingProfile')} />}
+              </div>
+            </div>
           </div>
-          {!this.state.editingProfile && !this.state.addingItem
-            && <button onClick={() => this.renderForm('editingProfile')}>Edit Profile</button>}
-          <div ref='editingProfile'>
-            {this.state.editingProfile
-              && <EditProfileForm onFinish={() => this.setFalse('editingProfile')} />}
-          </div>
-        </div>
         </div >
       )
     }
