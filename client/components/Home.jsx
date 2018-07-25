1b7296eb6f08c80de56d02f99edf4c46f548552b
reubenjh <rjharcourt@gmail.com>
2018-07-25 20:43:37 +1200

    darkened kend thingy

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -17,22 +17,22 @@ export class Home extends React.Component {
         <div className="container">
 
           <div className="section">
-            <h1 className='title is-1 has-text-centered'>Tool Pool is a community for peer-to-peer lending of odd household items.</h1>
+            <h1 className='title is-1 has-text-centered has-text-white'>Tool Pool is a community for peer-to-peer lending of odd household items.</h1>
           </div>
 
           <div className="section">
             <div className='columns'>
               <div className='column'>
-                <h1 className='title is-3'>About:</h1>
-                <p>The Tool Pool is a firendly gear sharing app where you can borrow bits a bobs from your communities. You can browse items that you need to borrow for those awkard one off odds and ends. </p>
+                <h1 className='title is-3 has-text-white'>About:</h1>
+                <p className='has-text-white'>The Tool Pool is a firendly gear sharing app where you can borrow bits a bobs from your communities. You can browse items that you need to borrow for those awkard one off odds and ends. </p>
               </div>
               <div className='column'>
-                <h1 className='title is-3'>Ethics:</h1>
-                <p>Our team here at The Tool Pool are committed to conducting business in an honest, responsible and ethical manner and in compliance with all applicable laws and regulations. We work with our suppliers and our customers to ensure an ethical and sustainable supply chain. </p>
+                <h1 className='title is-3 has-text-white'>Ethics:</h1>
+                <p className='has-text-white'>Our team here at The Tool Pool are committed to conducting business in an honest, responsible and ethical manner and in compliance with all applicable laws and regulations. We work with our suppliers and our customers to ensure an ethical and sustainable supply chain. </p>
               </div>
               <div className='column'>
-                <h1 className='title is-3'>Sustainability</h1>
-                <p>Tool Pool is commited to build on its achievements in the efficient management of energy, water and key material resources, and the minimisation of waste and emissions, and commits to continue to find new and innovative ways to demonstrate leadership in research, teaching, learning, operations, partnerships, capacity building and networking to advance sustainability.</p>
+                <h1 className='title is-3 has-text-white'>Sustainability</h1>
+                <p className='has-text-white'>Tool Pool is commited to build on its achievements in the efficient management of energy, water and key material resources, and the minimisation of waste and emissions, and commits to continue to find new and innovative ways to demonstrate leadership in research, teaching, learning, operations, partnerships, capacity building and networking to advance sustainability.</p>
               </div>
             </div>
           </div>
