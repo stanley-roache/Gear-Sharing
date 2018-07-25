e8124cadada650b742bb449050c14b628c962fe6
reubenjh <rjharcourt@gmail.com>
2018-07-25 16:16:25 +1200

    edited gearItem page and buttons, needs more modals

--- client/components/GearItem.jsx
+++ client/components/GearItem.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import scrollToComponent from 'react-scroll-to-component'
 
 import GearEdit from './GearEdit'
 import GearRequest from './GearRequest'
@@ -22,10 +21,6 @@ export class GearItem extends React.Component {
     renderForm() {
         this.setState({
             editingItem: true
-        })
-        scrollToComponent(this.refs.editForm, {
-            align: 'top',
-            duration: 1000
         })
     }
 
@@ -61,49 +56,73 @@ export class GearItem extends React.Component {
             const gearOwnerId = user_id
 
             return (
-                <div>
-                    <div className='hero'>
-                        <div className='hero-body'>
-                            <div className='container has-text-centered'>
+                <div className='section'>
+                    <div className='container'>
+                        <div className='columns is-multiline has-text-centered'>
+                            <div className='column is-12'>
                                 <h1 className='title is-1'>{name}</h1>
                             </div>
+
+                            <div className='column is-12'>
+                                <p>@placeholderino</p>
+                            </div>
+                        </div>
+                        <div className='columns box gear-card'>
+                            <div className='column is-6'>
+                                <h3 className='title is-3'>Description:</h3>
+                                <p>{description}</p>
+                                <br />
+                                <h5 className='title is-5' id='is-gear-item-subcategory1'>Availability:</h5>
+                                <p>{status}</p>
+                                <br />
+                                <h5 className='title is-5' id='is-gear-item-subcategory2'>Borrowing conditions:</h5>
+                                <p>{trustframework}</p>
+                                <br />
+
+                            </div>
+                            <div className='column is-6'>
+                                <img src={photo_url} />
+                            </div>
+                        </div>
+
+                        <div className='columns is-multiline is-centered'>
+                            <div className='column is-12'>
+                                {!this.props.auth.isAuthenticated
+                                    && <p className='has-text-centered'>Only registered users can request to borrow gear</p>}
+                                    
+                                    {this.props.auth.isAuthenticated && activeUserId === gearOwnerId && !this.state.editingItem
+                                        && <p className='has-text-centered'>This is your tool</p>}
+                                </div>
+                                <div className='column is-6'>
+
+                                    {!this.props.auth.isAuthenticated
+                                        && <button className='submit button is-centered is-black is-large' onClick={this.toLogin}>Login/Register</button>}
+
+                                    {this.props.auth.isAuthenticated && activeUserId === gearOwnerId && !this.state.editingItem
+                                        && <button className='submit button is-centered is-black is-large' onClick={() => this.renderForm()}>Edit this tool</button>}
+                                </div>
+                            </div>
+
+                            <div ref='editForm'>
+                                {this.state.editingItem
+                                    && <GearEdit item={thisGear} finish={() => this.cancelEdit()} />}
+                            </div>
+
+                            {this.props.auth.isAuthenticated && activeUserId !== gearOwnerId
+                                && <GearRequest onFinish={() => { }} gear_id={gearId} owner_id={gearOwnerId} requester_id={activeUserId} />}
+
                         </div>
                     </div>
-                <div className='columns box'>
-                    <div className='column is-7'>
-                        <p className='subtitle is-5'>{description}</p>
-                        <h3 className='subtitle is-4'>Avaliability: {status}</h3>
-                        <h3 className='subtitle is-4'>Trust Level: {trustframework}</h3>
-
-                        {!this.props.auth.isAuthenticated
-                            && <button onClick={this.toLogin}>Login to request this tool</button>}
-
-                        {this.props.auth.isAuthenticated && activeUserId === gearOwnerId && !this.state.editingItem
-                            && <button className='button' onClick={() => this.renderForm()}>Edit this tool</button>}
-
-                        <div ref='editForm'>
-                            {this.state.editingItem
-                                && <GearEdit item={thisGear} finish={() => this.cancelEdit()} />}
-                        </div>
-
-                        {this.props.auth.isAuthenticated && activeUserId !== gearOwnerId
-                            && <GearRequest onFinish={() => { }} gear_id={gearId} owner_id={gearOwnerId} requester_id={activeUserId} />}
-                    </div>
-                    <div className='column is-5'>
-                        <img className='imgset' src={photo_url} />
-                    </div>
-                </div>
-                </div>
-            )
+                    )
+                }
+            }
         }
-    }
-}
-
-const mapStateToProps = (state) => (
+        
+        const mapStateToProps = (state) => (
     {
-        auth: state.auth,
-        user: state.user,
-        gear: state.gear
-    }
-)
+                        auth: state.auth,
+                    user: state.user,
+                    gear: state.gear
+                }
+            )
 export default connect(mapStateToProps)(GearItem)
