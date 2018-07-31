12f10152512bf1205d69301d00459f52926f80ef
Rosie Burkett <burkettroz@gmail.com>
2018-08-01 11:06:34 +1200

    removed log and updated request from post to put

--- client/actions/gear.js
+++ client/actions/gear.js
@@ -74,10 +74,8 @@ export function editGearItem(item) {
   delete item.user_name
   return dispatch => {
     dispatch(editRequest())
-    return request('post', `gear/update/${item.id}`, item)
+    return request('put', `gear/update/${item.id}`, item)
       .then((res) => {
-        console.log('successgul updated');
-        
         dispatch(editGear(item))
       })
       .catch(err => {
